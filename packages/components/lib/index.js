8439d62b8b677fb79297ed778d5634e069f66798
mahesh-aet <mahesh.k@aeturnum.com>
2022-01-25 13:28:11 +0530

    mono-repo-poc-1

---
+++ packages/components/lib/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react'
+import Button from './button'
+
+// export default components;
+
+// function components() {
+// return <div>hello</div>
+// }
+
+module.exports = {
+    Button
+}
